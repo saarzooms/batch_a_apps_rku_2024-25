51570fec7f835e916690fe78e8be71628b43ec54
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-07-22 09:46:43 +0530

    todoscreen add, delete, iscompleted changes

--- todo_list/lib/screens/todo_screen.dart
+++ todo_list/lib/screens/todo_screen.dart
@@ -8,7 +8,10 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
-  List todos = [];
+  TextEditingController txtTitle = TextEditingController();
+  List todos = [
+    {'id': 1, 'title': 'Buy books', 'isCompleted': false}
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +26,7 @@ class _TodoScreenState extends State<TodoScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextField(
+                      controller: txtTitle,
                       decoration: InputDecoration(
                         hintText: 'Enter your task',
                       ),
@@ -30,30 +34,60 @@ class _TodoScreenState extends State<TodoScreen> {
                   ),
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    //to add or update logic into list
+                    if (txtTitle.text.isNotEmpty) {
+                      todos.add({
+                        'id': DateTime.now().toIso8601String(),
+                        'title': txtTitle.text,
+                        'isCompleted': false
+                      });
+                      //clear textfield
+                      txtTitle.clear();
+                      setState(() {});
+                    }
+                  },
                   icon: Icon(Icons.add),
                 ),
               ],
             ),
             Expanded(
               child: ListView.builder(
+                itemCount: todos.length,
                 itemBuilder: (context, index) {
+                  var todo = todos[index];
                   return Card(
                     child: CheckboxListTile(
                       controlAffinity: ListTileControlAffinity.leading,
-                      value: true,
-                      onChanged: (v) {},
-                      title: Text('Todos name'),
+                      value: todo['isCompleted'],
+                      onChanged: (v) {
+                        todo['isCompleted'] = v;
+                        setState(() {});
+                      },
+                      title: Text('${todo['title']} (${todo['id']})',
+                          style: TextStyle(
+                              decoration: todo['isCompleted']
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                              color: todo['isCompleted']
+                                  ? Colors.red
+                                  : Colors.black)),
                       secondary: SizedBox(
                         width: 80,
                         child: Row(
                           children: [
                             IconButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                txtTitle.text = todo['title'];
+                              },
                               icon: Icon(Icons.edit),
                             ),
                             IconButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                //logic to delete the task
+                                todos.removeAt(index);
+                                setState(() {});
+                              },
                               icon: Icon(Icons.delete),
                             ),
                           ],
