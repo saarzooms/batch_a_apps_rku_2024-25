f1bff3a540497f7873df0af0ccc4f2e88c0bf248
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-07-30 09:23:36 +0530

    added animated bottom navigation bar

--- e_com_app/lib/screens/master_screen.dart
+++ e_com_app/lib/screens/master_screen.dart
@@ -1,6 +1,11 @@
 import 'dart:developer';
 
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_screen.dart';
+import 'dashboard_screen.dart';
+import 'product_screen.dart';
 
 class MasterScreen extends StatefulWidget {
   const MasterScreen({super.key});
@@ -11,26 +16,53 @@ class MasterScreen extends StatefulWidget {
 
 class _MasterScreenState extends State<MasterScreen> {
   int currIndex = 0;
+  List pages = [
+    DashboardScreen(),
+    ProductScreen(),
+    CartScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          // backgroundColor: Colors.green,
-          currentIndex: currIndex,
-          onTap: (index) {
-            log('clicked on $index');
-            currIndex = index;
-            setState(() {});
-          },
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.inventory_2), label: 'Product'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart), label: 'Cart'),
-            // BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            // BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          ]),
+      body: pages[currIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        //params
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: [
+          Icons.home,
+          Icons.inventory_2,
+          Icons.shopping_cart,
+          Icons.settings,
+        ],
+        activeIndex: currIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (index) => setState(() => currIndex = index),
+        //other params
+      ),
+      // BottomNavigationBar(
+      //     // backgroundColor: Colors.green,
+      //     currentIndex: currIndex,
+      //     onTap: (index) {
+      //       log('clicked on $index');
+      //       currIndex = index;
+      //       setState(() {});
+      //     },
+      //     items: [
+      //       BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      //       BottomNavigationBarItem(
+      //           icon: Icon(Icons.inventory_2), label: 'Product'),
+      //       BottomNavigationBarItem(
+      //           icon: Icon(Icons.shopping_cart), label: 'Cart'),
+      //       // BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      //       // BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      //     ]),
     );
   }
 }
